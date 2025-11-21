f35e67ee6e3f8bfeb29be3d2c4ed5f39c7356308
Faisal <softtechrana05@gmail.com>
2025-11-21 21:03:49 +0500

    pdo db connection update

--- pdo.php
+++ pdo.php
@@ -1,4 +1,21 @@
 <?php
-echo "This is for pdo db connection";
+// This is for pdo db connection;
+$host = "localhost";
+$username = "root";
+$password = null;
+try{
+    $conn = new PDO("mysql:host=$host;dbname=my_db1",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //echo "conection done";
+}catch(PDOException $err){
+    //echo "connection failed $err->getMessage()";
+}
+// Connection Made
+// Put Mysql Query in Connection
+echo "<br>";
+$Q = $conn->query("show tables");
+while($v=$Q->fetch(PDO::FETCH_NUM)){
+    // print_r($v);
+}
 
 ?>
