54462be84d45c8f462b0b94a089a634c83f24bfe
Faisal <softtechrana05@gmail.com>
2025-11-24 23:55:54 +0500

    updated Loops.php file

--- Loops.php
+++ Loops.php
@@ -31,6 +31,16 @@ $name = "Faisal Siraj";
 for($a = 0; $a < strlen($name); $a++){
     echo $name[$a]."<br>";
 }
+// Find max and min values from an array (using loop only)
+$arr = [12, 45, 3, 67, 23];
+$max = $arr[0];
+$min = $arr[0];
+foreach ($arr as $num) {
+    if ($num > $max) $max = $num;
+    if ($num < $min) $min = $num;
+}
+echo "Max: $max<br>";
+echo "Min: $min<br>";
 
 
 ?>
